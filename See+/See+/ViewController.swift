70b562fe4aa13a3c6c7bbe6e6a00c4789e6938ba
Quentin c <quentinclement@macbook-pro-de-quentin.local>
2018-06-05 14:38:20 +0200

    develop : First commit (create project)

---
+++ See+/See+/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  See+
+//
+//  Created by Quentin CLEMENT on 05/06/2018.
+//  Copyright © 2018 Quentin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
